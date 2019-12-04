8f3127961354e6c8115b07e2d5c9f53ed49e4f1e
praveen <hemadripraveen@gmail.com>
2019-12-04 23:46:20 +0530

    testing all java 8 features

---
+++ java8/src/main/java/com/otsi/streams/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.otsi.streams;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
